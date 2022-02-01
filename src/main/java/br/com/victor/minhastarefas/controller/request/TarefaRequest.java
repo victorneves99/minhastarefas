2e412925d554424d61851f9f742857448f12d306
victorneves <victorneves99@gmail.com>
2022-02-01 16:49:22 -0300

    isolar o escopo das entidades dos objetos e criando validações nos DTO's

--- src/main/java/br/com/victor/minhastarefas/controller/request/TarefaRequest.java
+++ src/main/java/br/com/victor/minhastarefas/controller/request/TarefaRequest.java
@@ -2,12 +2,27 @@ package br.com.victor.minhastarefas.controller.request;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class TarefaRequest {
 
     private Integer id;
+    @NotBlank(message = "{tarefa.descricao.not-blank}")
+    @Size(min = 5,max = 150,message = "{tarefa.descricao.size}")
     private String descricao;
+    @FutureOrPresent(message = "{tarefa.descricao.future-or-present}")
     private LocalDate dataEntrega;
+
+    @NotNull(message = "{tarefa.categoria.not-null}")
+    @Min(value = 1,message = "{tarefa.categoria.min}")
     private Integer categoriaId;
+
+    @NotNull(message = "{tarefa.usuario.not-null}")
+    @Min(value = 1,message = "{tarefa.usuario.min}")
     private Integer usuarioId;
 
     public Integer getId() {
