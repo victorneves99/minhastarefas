aaf16de534a768fb56d34ae11fc66454d9401033
victorneves <victorneves99@gmail.com>
2022-02-15 16:04:08 -0300

    criando autenticacao e autorizacao em memoria .

--- src/main/java/br/com/victor/minhastarefas/security/WebSecurityConfig.java
+++ src/main/java/br/com/victor/minhastarefas/security/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package br.com.victor.minhastarefas.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,6 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PATHS = new String[] { "/tarefa/**", "/categoria/**", "/usuario/**" };
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -33,14 +37,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     // autenticaçao
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("usuario")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER");
 
     }
 
     // autorizaçao
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+
+        http.csrf().disable().cors()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers(HttpMethod.POST, PATHS)
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, PATHS)
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, PATHS)
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, PATHS)
+                .hasAnyRole("ADMIN", "USER")
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
     }
 
 }
